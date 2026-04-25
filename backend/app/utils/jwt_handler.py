acb6c26585a475d24f879a765dadb8a0f573bb3d
wilsonkayke <lenevangelista86@gmail.com>
2026-04-25 12:51:53 -0300

    fix configuração de JWT

--- backend/app/utils/jwt_handler.py
+++ backend/app/utils/jwt_handler.py
@@ -1,17 +1,17 @@
+import os
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
-SECRET_KEY = "sua_chave_super_secreta"
+SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")  # fallback local
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire}) 
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
@@ -23,4 +23,4 @@ def verify_token(token: str):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
-        return None
+        return None 
