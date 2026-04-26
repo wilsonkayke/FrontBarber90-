4ec787aca4e6dcbe02dbe8f6bb743f77249a2e8e
wilsonkayke <lenevangelista86@gmail.com>
2026-04-25 22:27:04 -0300

    fix page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/login");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+
+  return <p>Redirecionando...</p>;
 }
