4c1f65c15a220beb4dec55d0f6ec7fd899fe6a63
wilsonkayke <lenevangelista86@gmail.com>
2026-05-09 16:06:36 -0300

    refactor login

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -23,7 +23,7 @@ export default function LoginPage() {
     setMsgSucesso("");
 
     try {
-      const response = await fetch(`${API_URL}/auth/login`, {
+      const response = await fetch("https://barber-edxf.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
