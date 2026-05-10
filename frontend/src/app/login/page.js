7d2f5b6b97010a0ee39b9c306f8c84ebff880780
wilsonkayke <lenevangelista86@gmail.com>
2026-05-10 18:08:04 -0300

    refactor login

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,11 +1,9 @@
 "use client";
 
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -23,16 +21,19 @@ export default function LoginPage() {
     setMsgSucesso("");
 
     try {
-      const response = await fetch("https://barber-edxf.onrender.com/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const response = await fetch(
+        "https://barber-edxf.onrender.com/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            senha,
+          }),
         },
-        body: JSON.stringify({
-          email,
-          senha,
-        }),
-      });
+      );
 
       const data = await response.json();
 
@@ -55,95 +56,92 @@ export default function LoginPage() {
       setMsgSucesso("Login realizado com sucesso!");
 
       setTimeout(() => {
-        if (data.user.role === "admin") {
+        if (data.user.role?.trim().toLowerCase() === "admin") {
           router.push("/admin");
         } else {
           router.push("/agenda");
         }
-      }, 1000);
-
+      }, 1000); 
     } catch (error) {
       console.error(error);
       setMsgErro("Erro ao conectar com o servidor");
     }
-  }; 
+  };
 
   return (
-  <main className="flex items-center justify-center min-h-screen bg-linear-to-r from-blue-100 to-gray-800 px-4">
-    
-    <div className="bg-white shadow-xl rounded-2xl p-6 sm:p-8 w-full max-w-md">
-      
-      <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center text-gray-800">
-        Entrar
-      </h1>
+    <main className="flex items-center justify-center min-h-screen bg-linear-to-r from-blue-100 to-gray-800 px-4">
+      <div className="bg-white shadow-xl rounded-2xl p-6 sm:p-8 w-full max-w-md">
+        <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center text-gray-800">
+          Entrar
+        </h1>
 
-      {msgErro && (
-        <div className="bg-red-100 text-red-600 p-2 sm:p-3 mb-4 rounded-lg text-center text-sm">
-          {msgErro}
+        {msgErro && (
+          <div className="bg-red-100 text-red-600 p-2 sm:p-3 mb-4 rounded-lg text-center text-sm">
+            {msgErro}
+          </div>
+        )}
+
+        {msgSucesso && (
+          <div className="bg-green-100 text-green-600 p-2 sm:p-3 mb-4 rounded-lg text-center text-sm">
+            {msgSucesso}
+          </div>
+        )}
+
+        {/* Email */}
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Email
+          </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu email"
+            className="w-full border border-gray-300 rounded-lg p-2.5 text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+          />
         </div>
-      )}
 
-      {msgSucesso && (
-        <div className="bg-green-100 text-green-600 p-2 sm:p-3 mb-4 rounded-lg text-center text-sm">
-          {msgSucesso}
+        {/* Senha */}
+        <div className="mb-6 relative">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Senha
+          </label>
+
+          <input
+            type={mostrarSenha ? "text" : "password"}
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Digite sua senha"
+            className="w-full border border-gray-300 rounded-lg p-2.5 pr-11 text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+          />
+
+          <button
+            type="button"
+            onClick={() => setMostrarSenha(!mostrarSenha)}
+            className="absolute right-3 top-9.5 text-gray-500 hover:text-gray-700"
+          >
+            {mostrarSenha ? "🙈" : "👁"}
+          </button>
         </div>
-      )}
 
-      {/* Email */}
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Email
-        </label>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Digite seu email"
-          className="w-full border border-gray-300 rounded-lg p-2.5 text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
-        />
-      </div>
-
-      {/* Senha */}
-      <div className="mb-6 relative">
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Senha
-        </label>
-
-        <input
-          type={mostrarSenha ? "text" : "password"}
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Digite sua senha"
-          className="w-full border border-gray-300 rounded-lg p-2.5 pr-11 text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
-        />
-
+        {/* Botão */}
         <button
-          type="button"
-          onClick={() => setMostrarSenha(!mostrarSenha)}
-          className="absolute right-3 top-9.5 text-gray-500 hover:text-gray-700"
+          onClick={entrar}
+          className="w-full bg-blue-600 text-white py-2.5 rounded-lg text-sm sm:text-base hover:bg-blue-700 active:scale-95 transition"
         >
-          {mostrarSenha ? "🙈" : "👁"}
+          Entrar
         </button>
-      </div>
 
-      {/* Botão */}
-      <button
-        onClick={entrar}
-        className="w-full bg-blue-600 text-white py-2.5 rounded-lg text-sm sm:text-base hover:bg-blue-700 active:scale-95 transition"
-      >
-        Entrar
-      </button>
-
-      {/* Link */}
-      <div className="text-center mt-4">
-        <a
-          href="/cadastro"
-          className="text-blue-600 hover:underline font-medium text-sm sm:text-base"
-        >
-          Cadastrar-se
-        </a>
+        {/* Link */}
+        <div className="text-center mt-4">
+          <a
+            href="/cadastro"
+            className="text-blue-600 hover:underline font-medium text-sm sm:text-base"
+          >
+            Cadastrar-se
+          </a>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
   );
-} 
+}
