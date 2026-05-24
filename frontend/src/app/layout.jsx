6746f3a04727c41f41726d5888842c9fcfb449dc
wilsonkayke <lenevangelista86@gmail.com>
2026-05-24 17:12:09 -0300

    feat: Speed Insights

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Barbearia",
@@ -7,11 +8,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  <Analytics />
-  
-  return (
+  return ( 
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
-}
+} 
