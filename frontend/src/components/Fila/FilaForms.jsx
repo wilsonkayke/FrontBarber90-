595f4a5371437da1b24d3171efa520f414f6aaa3
wilsonkayke <lenevangelista86@gmail.com>
2026-05-24 21:04:28 -0300

    feat: carousel fila

--- frontend/src/components/Fila/FilaForms.jsx
+++ frontend/src/components/Fila/FilaForms.jsx
@@ -1,9 +1,32 @@
 'use client';
 
+import { useState } from "react";
+
 export default function FilaForms({
   fila,
   exit,
 }) {
+
+  const imagens = [
+    "/imagens/jaca.jpg",
+    "/imagens/Reflexo.jpg",
+    "/imagens/Nevou.jpg",
+    "/imagens/Moicano.jpg",
+  ]
+
+  const [index, setIndex] = useState(0);
+
+  const proximo = () => {
+    setIndex((prev) =>
+      prev === imagens.length - 1 ? 0 : prev + 1
+    );
+  };
+
+  const anterior = () => {
+    setIndex((prev) =>
+      prev === 0 ? imagens.length - 1 : prev - 1
+    );
+  };
 
   return (
     <main
@@ -23,7 +46,7 @@ export default function FilaForms({
       "
     >
 
-      <div className="bg-gray-100/95 shadow-2xl rounded-2xl p-8 w-full max-w-md backdrop-blur-sm">
+      <div className="bg-gray-100/95 flex flex-col shadow-2xl rounded-2xl p-5 mb-50 w-full max-w-md backdrop-blur-sm gap-4">
 
         <h1 className="text-2xl font-bold text-center mb-6">
           Acompanhamento da Fila
@@ -76,6 +99,42 @@ export default function FilaForms({
         )}
 
       </div>
+      
+      {/* Imagem Carrossel */}
+      <div className="flex
+        flex-col
+        items-center
+        gap-3
+        absolute
+        bottom-5
+        right-5">
+
+        <img
+          src={imagens[index]}
+          alt="carousel"
+          className="w-48 h-48 object-cover rounded-full shadow-lg"
+        />
+
+        <div className="flex gap-2">
+          <button
+            onClick={anterior}
+            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400 transition"
+          >
+            Anterior
+          </button>
+          <button
+            onClick={proximo}
+            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400 transition"
+          >
+            {/*→*/}
+            Próximo
+          </button>
+        </div>
+
+        
+      </div>
+        
+
     </main>
   );
 }
